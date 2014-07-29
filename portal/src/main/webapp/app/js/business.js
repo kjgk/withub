361003afacdeb8f5583af16b5380aedac36dc90d
guyb <guyb@withub.com.cn>
2014-07-29 16:00:08 +0800

    New changelist

--- portal/src/main/webapp/app/js/business.js
+++ portal/src/main/webapp/app/js/business.js
@@ -29,10 +29,10 @@ angular.module('app.business', ['withub'])
     .factory('BusinessService', [
         function () {
             var businessList = [
-                {id: '111', title: '系统集成', subTitle: '以市场为先导 以集成倡导服务', titleEn: 'System integration', content: '依靠交大的人才优势，将产品技术进步与应用需求发展有机结合，为客户提供计算机网络信息系统、数据存储及灾备系统信息安全系统的总体规划设计及建设；智能化弱点系统总包及专业化多媒体会议系统、安防监控系统、综合布线、数据中心机房系统的规划设计及建设。', src: 'static/images/b_1.png', src2: 'static/images/b_5.png'},
-                {id: '112', title: '软件开发', subTitle: '优质产品 高效服务 以人为本 客户至上', titleEn: 'Software', content: '软件开发和咨询业务提供专业的行业解决方案和产品。客户群涉及法院、医疗、教育、政府、金融行业以及众多大型企业；拥有完善的产品系列，涵盖区域医疗、智慧养老、热线诉服、智能监控、商业智能、电子政务、电子商务、ERP等应用方向。软件提供完善的售后服务保障体系。让用户“用的放心，用的安心”是我们秉承的理念。', src: 'static/images/b_2.png', src2: 'static/images/b_6.png'},
-                {id: '113', title: '解决方案', subTitle: '从传输到控制，从播放到特效显示，提供全系列多媒体创新展示解决方案', titleEn: 'Solutions', content: '公司致力于多媒体专业显示领域的系列软硬件产品研发和应用，自行研发的Media Master系列产品能满足智能播放、特种打拼显示、媒体控制及无线传输等众多显示需求，可为文化创意和内容制作提供个性化的播控平台，为多种多路计算机和视频信号的协同管理及使用单位的决策辅助系统提供交换、处理和交互的显示平台。', src: 'static/images/b_3.png', src2: 'static/images/b_7.png'},
-                {id: '114', title: '产品分销', subTitle: '专业第一 服务至上', titleEn: 'Product distribution', content: '公司笔记本产品分销业务，采取零售、行业、分销三种主要方式相结合的销售模式。经营的主力品牌有华硕笔记本电脑，是上海地区最大的分销商，并多次得到厂家的奖励，2005年7月获得最快成长奖、2006年7月至2010年7月一直获得最佳业绩奖，并于2011年7月获得最高奖项金钟奖。', src: 'static/images/b_4.png', src2: 'static/images/b_8.png'}
+                {id: '111', title: '系统集成', subTitle: '以市场为先导 以集成倡导服务', titleEn: 'System integration', content: '依靠交大的人才优势，将产品技术进步与应用需求发展有机结合，为客户提供计算机网络信息系统、数据存储及灾备系统信息安全系统的总体规划设计及建设；智能化弱点系统总包及专业化多媒体会议系统、安防监控系统、综合布线、数据中心机房系统的规划设计及建设。', src: 'static/images/b_1.jpg', src2: 'static/images/b_5.jpg'},
+                {id: '112', title: '软件开发', subTitle: '优质产品 高效服务 以人为本 客户至上', titleEn: 'Software', content: '软件开发和咨询业务提供专业的行业解决方案和产品。客户群涉及法院、医疗、教育、政府、金融行业以及众多大型企业；拥有完善的产品系列，涵盖区域医疗、智慧养老、热线诉服、智能监控、商业智能、电子政务、电子商务、ERP等应用方向。软件提供完善的售后服务保障体系。让用户“用的放心，用的安心”是我们秉承的理念。', src: 'static/images/b_2.jpg', src2: 'static/images/b_6.jpg'},
+                {id: '113', title: '解决方案', subTitle: '从传输到控制，从播放到特效显示，提供全系列多媒体创新展示解决方案', titleEn: 'Solutions', content: '公司致力于多媒体专业显示领域的系列软硬件产品研发和应用，自行研发的Media Master系列产品能满足智能播放、特种打拼显示、媒体控制及无线传输等众多显示需求，可为文化创意和内容制作提供个性化的播控平台，为多种多路计算机和视频信号的协同管理及使用单位的决策辅助系统提供交换、处理和交互的显示平台。', src: 'static/images/b_3.jpg', src2: 'static/images/b_7.jpg'},
+                {id: '114', title: '产品分销', subTitle: '专业第一 服务至上', titleEn: 'Product distribution', content: '公司笔记本产品分销业务，采取零售、行业、分销三种主要方式相结合的销售模式。经营的主力品牌有华硕笔记本电脑，是上海地区最大的分销商，并多次得到厂家的奖励，2005年7月获得最快成长奖、2006年7月至2010年7月一直获得最佳业绩奖，并于2011年7月获得最高奖项金钟奖。', src: 'static/images/b_4.jpg', src2: 'static/images/b_8.jpg'}
             ];
             return {
                 getBusinessList: function () {
--- portal/src/main/webapp/app/js/home.js
+++ portal/src/main/webapp/app/js/home.js
@@ -11,15 +11,16 @@ angular.module('app.home', ['withub'])
         }
     ])
     .controller('HomeCtrl', [
-        '$scope', 'BusinessService'
-        , function ($scope, BusinessService) {
+        '$scope', 'BusinessService', 'ContentService'
+        , function ($scope, BusinessService, ContentService) {
 
             /* 公司公告 */
-            $scope.noticeList1 = [
-                {eventTime: '2014-07-07 16:22', title: '发出通函后重大资料 / 在股东批准的情況下重选或委任董事'},
-                {eventTime: '2014-07-07 16:22', title: '董事名单和他们的地位和作用'},
-                {eventTime: '2014-07-07 16:22', title: '股东周年大会的結果 / 在股东批准的情況下重选或委任董事 / 更换董事或重要行政职能或职责'}
-            ]
+
+
+            ContentService.fetchContentList(101).then(function (result) {
+                $scope.noticeList1 = [result.items[0], result.items[1] , result.items[2]]
+            });
+
             $scope.noticeList2 = [
                 {eventTime: '2014-07-07 16:22', title: '董事名单和他们的地位和作用'},
                 {eventTime: '2014-07-07 16:22', title: '股东周年大会的結果 / 在股东批准的情況下重选或委任董事 / 更换董事或重要行政职能或职责'},
@@ -31,10 +32,10 @@ angular.module('app.home', ['withub'])
 
             /* 员工天地 */
             $scope.employeeWorldList = [
-                {title: '员工天地', src: 'static/images/e_1.png', url: ''},
-                {title: '企业文化', src: 'static/images/e_2.png', url: ''},
-                {title: '团队精神', src: 'static/images/e_4.png', url: ''},
-                {title: '慧谷人', src: 'static/images/e_3.png', url: ''}
+                {title: '员工天地', src: 'static/images/e_1.jpg', url: ''},
+                {title: '企业文化', src: 'static/images/e_2.jpg', url: ''},
+                {title: '团队精神', src: 'static/images/e_4.jpg', url: ''},
+                {title: '慧谷人', src: 'static/images/e_3.jpg', url: ''}
             ]
         }
 
